0a235b7f39d72ecf09405838d80e7fa6bb7521f9
antorhasan <kaziantorhasan@gmail.com>
2020-01-04 00:51:42 +0600

    need to normalize

--- torch_model.py
+++ torch_model.py
@@ -37,8 +37,9 @@ class Model(nn.Module):
         self.conv6 = nn.Conv2d(32,64,(1,3), padding=0) """
 
         self.lstm = nn.LSTM(745*3,20,num_layers=1,batch_first=True)
-
+        self.dropout1 = nn.Dropout(0.25)
         self.fc1 = nn.Linear(20, 2)
+        self.dropout2 = nn.Dropout(0.5)
 
     def forward(self, inputs):
         """ x = F.relu(self.conv1(inputs))
@@ -56,8 +57,13 @@ class Model(nn.Module):
         h0 = torch.zeros(1, batch_size, 20).cuda()
         c0 = torch.zeros(1, batch_size, 20).cuda()
         x, (hn, cn) = self.lstm(x, (h0, c0))
-        x = self.fc1(x)
         x = x[:,-1,:]
+        #print(x.size())
+        #print(asd)
+        #x = self.dropout1(x)
+        x = self.fc1(x)
+        #x = self.dropout2(x)
+        
         #print(x.size())
         #print(asd)
         return x
@@ -65,21 +71,36 @@ class Model(nn.Module):
 
 
 batch_size = 4
-EPOCHS = 20
-lr_rate = .0001
+EPOCHS = 100
+lr_rate = .001
 in_seq_num = 29
+val_batch_size = 4
+output_at = 10
 
 
 
 dataset = tf.data.TFRecordDataset('./data/tfrecord/pix_img.tfrecords')
 dataset = dataset.map(_parse_function)
-#dataset = dataset.shuffle(100)
+dataset = dataset.shuffle(200)
 dataset = dataset.batch(batch_size, drop_remainder=True)
+
+dataset_val = tf.data.TFRecordDataset('./data/tfrecord/pix_img.tfrecords')
+dataset_val = dataset_val.map(_parse_function)
+dataset_val = dataset_val.batch(val_batch_size, drop_remainder=True)
+
 
 use_cuda = not False and torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
 model = Model().to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=lr_rate)
+
+writer = SummaryWriter(log_dir='./data/runs/')
+
+test_list = []
+
+#checkpoint = torch.load('./data/model/first.pt')
+#model.load_state_dict(checkpoint['model_state_dict'])
+#optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
 for epoch in range(EPOCHS):
     model.train()
@@ -114,4 +135,74 @@ for epoch in range(EPOCHS):
     avg_epoch_loss = epoch_loss / counter
     template = 'Epoch {}, Train Loss: {}'
     print(template.format(epoch+1,avg_epoch_loss))
-    #writer.add_scalar('Loss/train', run_t, epoch)
+    writer.add_scalar('Loss/train', avg_epoch_loss, epoch+1)
+
+    """ if epoch % 20 == 0:
+        torch.save({
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict()
+                }, './data/model/shuffle_test.pt') """ 
+
+
+    model.eval()
+    val_epoch_loss = 0
+    count = 0
+    
+    with torch.no_grad():
+        for img, msk in dataset_val:
+            img = np.reshape(img, (val_batch_size,32,745,3))
+            msk = np.reshape(msk, (val_batch_size,32,2))
+            img = img[:,1:in_seq_num+1,:,:]
+            msk = msk[:,in_seq_num+1:in_seq_num+2,:]
+            
+            img = torch.Tensor(img).cuda()
+            msk = torch.Tensor(msk).cuda()
+            msk = torch.reshape(msk, (val_batch_size,-1))
+
+            pred = model(img)
+            loss = F.mse_loss(pred, msk)
+            val_epoch_loss = val_epoch_loss + loss
+            #print( epoch+1 % 3)
+            if (epoch+2) % output_at == 0:
+                msk = msk.cpu().detach().numpy()
+                pred = pred.cpu().detach().numpy()
+                line = np.zeros((val_batch_size,745,3))
+                for k in range(val_batch_size):
+                    line[k,int(msk[k,0]),:] = [255,255,255]
+                    line[k,int(msk[k,1]),:] = [255,255,255]
+
+                    line[k,int(pred[k,0]),:] = [0,0,255]
+                    line[k,int(pred[k,1]),:] = [0,0,255]
+            
+                test_list.append(line)
+
+            
+            
+            count += 1
+
+    if (epoch+1) % output_at == 0:
+        #print(len(test_list))
+        output = np.asarray(test_list, dtype=np.uint8)
+        output = np.reshape(output, (-1,745,3))
+        #output = np.reshape(output, (len(test_list)*val_batch_size,745,3))
+        cv2.imwrite('./data/output/' + str(epoch+1) +'.png',output)
+        #print(output.shape)
+        #print(asd)
+        test_list = []
+
+    avg_val_epoch_loss = val_epoch_loss / count
+    template = 'Epoch {}, Val Loss: {}'
+    print(template.format(epoch+1,avg_val_epoch_loss))
+    writer.add_scalar('Loss/Val', avg_val_epoch_loss, epoch+1)
+
+
+    
+
+
+
+
+
+
+
+
+
