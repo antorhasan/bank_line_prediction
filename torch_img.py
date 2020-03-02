6d1dfe2655b5c32cc7cf34e1b621868353b4cc08
antorhasan <antor@ra.cse.buet.ac.bd>
2020-03-02 21:06:47 +0600

    starting wandb integration

--- torch_img.py
+++ torch_img.py
@@ -17,52 +17,73 @@ cv2.waitKey(0)
 cv2.destroyAllWindows()
 print(asd)
  """
-path = './data/img/finaljan/'
-data_list = [f for f in listdir(path) if isfile(join(path, f))]
-for i in range(len(data_list)):
-    data_list[i] = int(data_list[i].split('.')[0])
-data_list.sort()
-#print(data_list)
-#print(asd)
-def _bytes_feature(value):
-    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
-def write_data():
-    writer = tf.io.TFRecordWriter('./data/tfrecord/'+ 'pix_img' +'.tfrecords')
-    for i in range(2047):
-        print(i)
-        seq_list = []
-        msk_list = []
-        for j in range(32):
-            img = cv2.imread(path + str(data_list[j]) + '.png', 1)
-            img = img[1:2048,385:1130,:]
-            img = img[i,:,:]
-            img = img/255
-            seq_list.append(img)
+def write_pix_img(mode):
+    '''writes data into a tf record from rgb,infra images along with labels from binary image with
+    lines as two pixel for both banks per vertical length'''
 
-            msk = cv2.imread('./data/img/lines/' + str(data_list[j]) + '.png', 0)
-            msk = msk[1:2048,385:1130]
-            msk = msk[i,:]
-            indx = np.argwhere(msk==255)
-            indx = np.reshape(indx, (2,))
-            msk_list.append(indx)
-            #print(indx)
-            #print(asd)
+    offset_left = 385
+    offset_right = 1130
 
-        seq_list = np.asarray(seq_list, dtype=np.float32)
-        msk_list = np.asarray(msk_list, dtype=np.float32)
+    path = './data/img/finaljan/'
+    path_infra = './data/img/infra1/'
+    data_list = [f for f in listdir(path) if isfile(join(path, f))]
+    for i in range(len(data_list)):
+        data_list[i] = int(data_list[i].split('.')[0])
+    data_list.sort()
+    #print(data_list)
+    #print(asd)
+    def _bytes_feature(value):
+        return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
-        feature = {
-            'image': _bytes_feature(seq_list.tostring()),
-            'msk': _bytes_feature(msk_list.tostring())
-        }
-        example = tf.train.Example(
-            features=tf.train.Features(feature=feature))
-        writer.write(example.SerializeToString())
+    def write_data():
+        writer = tf.io.TFRecordWriter('./data/tfrecord/'+ 'pix_img_all' +'.tfrecords')
+        for i in range(2047):
+            print(i)
+            seq_list = []
+            msk_list = []
+            for j in range(32):
+                img = cv2.imread(path + str(data_list[j]) + '.png', 1)
+                img = img[1:2048,offset_left:offset_right,:]
+                img = img[i,:,:]
+                img = img/255
+
+                if mode=='infra':
+                    img_i = cv2.imread(path_infra + str(data_list[j]) + '.png', 1)
+                    img_i = img_i[1:2048,offset_left:offset_right,:]
+                    img_i = img_i[i,:,:]
+                    img_i = img_i/255
+                
+                img = np.concatenate((img,img_i), axis=1)
+                
+                seq_list.append(img)
+
+                msk = cv2.imread('./data/img/lines/' + str(data_list[j]) + '.png', 0)
+                msk = msk[1:2048,offset_left:offset_right]
+                msk = msk[i,:]
+                indx = np.argwhere(msk==255)
+                indx = np.reshape(indx, (2,))
+                msk_list.append(indx)
+                #print(indx)
+                #print(asd)
+
+            seq_list = np.asarray(seq_list, dtype=np.float32)
+            msk_list = np.asarray(msk_list, dtype=np.float32)
+
+            feature = {
+                'image': _bytes_feature(seq_list.tostring()),
+                'msk': _bytes_feature(msk_list.tostring())
+            }
+            example = tf.train.Example(
+                features=tf.train.Features(feature=feature))
+            writer.write(example.SerializeToString())
 
 
-    writer.close()
-    sys.stdout.flush()    
+        writer.close()
+        sys.stdout.flush()    
 
 
-write_data()
+    write_data()
+
+if __name__ == "__main__" :
+    write_pix_img(mode='infra')
