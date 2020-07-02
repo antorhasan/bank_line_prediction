805da919eaaaedc9aafe3a223e58d5a20b8fd690
antorhasan <kaziantorhasan@gmail.com>
2020-07-03 02:44:16 +0600

    moved to tensorboard

--- fin_model.py
+++ fin_model.py
@@ -9,7 +9,6 @@ from os import listdir
 from os.path import isfile, join
 import os
 import sys
-#import wandb
 from torch.utils.tensorboard import SummaryWriter
 from models import CNN_Model
 from sklearn.metrics import mean_absolute_error, precision_score, recall_score, confusion_matrix, f1_score
@@ -18,12 +17,6 @@ import matplotlib.pyplot as plt
 import itertools
 from scipy.signal import savgol_filter
 import optuna
-
-#os.environ["WANDB_API_KEY"] = 'local-1479f9a9f8553920b500edc5cba063a6efb261f0'
-#os.environ["WANDB_BASE_URL"] = "http://localhost:8080"
-
-#os.environ["WANDB_API_KEY"] = 'd74313ef4600d2878ab52142cfcea8d314610c67'
-#os.environ["WANDB_BASE_URL"] = "https://api.wandb.ai"
 
 
 def _parse_function_(example_proto):
@@ -45,9 +38,9 @@ def _parse_function_(example_proto):
     return input_tensor, reg_coor, bin_label, year_id
 
 
-def plt_conf_mat(conf_mat, title):
+def plt_conf_mat(conf_mat, title, writer):
     normalize = True
-    plt.figure(clear=True)
+    mat_fig = plt.figure(clear=True)
     cmap = plt.get_cmap('Blues')
     plt.imshow(conf_mat, interpolation='nearest', cmap=cmap)
     plt.title(title)
@@ -77,6 +70,7 @@ def plt_conf_mat(conf_mat, title):
     plt.xlabel('Predicted label')
     
     #wandb.log({title: plt})
+    writer.add_figure(title, mat_fig)
     plt.close()
 
 def regress_erro(act_err_bin, act_reg, pred_reg, iter_num, side,writer, val_img_ids,epoch):
@@ -124,19 +118,14 @@ def calc_fscore(iter_num, actual_list, prev_actual_list, pred_list, epoch,writer
     right_mae = regress_erro(actual_ers_rht, act_right, pred_right, iter_num, 'right',writer,val_img_ids,epoch)
 
     avg_mae = (left_mae + right_mae)/2
-    #wandb.log({'pos_mae_for_actual_erosion'+str(val_img_ids[iter_num]) : avg_mae})
     writer.add_scalar('pos_mae_for_actual_erosion'+str(val_img_ids[iter_num]), avg_mae, epoch+1)
 
     pred_ers_lft = np.reshape(np.where(pred_left<prev_left, 1, 0),(pred_left.shape[0],1))
     pred_ers_rht = np.reshape(np.where(pred_right>prev_right, 1, 0),(pred_right.shape[0],1))
 
-    conf_mat_lft = confusion_matrix(actual_ers_lft, pred_ers_lft)
+    """ conf_mat_lft = confusion_matrix(actual_ers_lft, pred_ers_lft)
     conf_mat_rht = confusion_matrix(actual_ers_rht, pred_ers_rht)
-    combined_conf = conf_mat_lft + conf_mat_rht
-
-    #plt_conf_mat(conf_mat_lft, str(val_img_ids[iter_num])+'_conf_mat_left')
-    #plt_conf_mat(conf_mat_rht, str(val_img_ids[iter_num])+'_conf_mat_right')
-    #plt_conf_mat(combined_conf, str(val_img_ids[iter_num])+'_combined_conf_mat')
+    combined_conf = conf_mat_lft + conf_mat_rht """
 
     y_true = np.concatenate((actual_ers_lft,actual_ers_rht), axis = 0)
     y_pred = np.concatenate((pred_ers_lft,pred_ers_rht), axis = 0)
@@ -145,54 +134,72 @@ def calc_fscore(iter_num, actual_list, prev_actual_list, pred_list, epoch,writer
     recall_comb = recall_score(y_true, y_pred, average='binary')
     f1_comb = f1_score(y_true, y_pred, average='binary')
     
-    #wandb.log({'precision_'+ str(val_img_ids[iter_num]) : precision_comb})
-    #wandb.log({'recall_'+ str(val_img_ids[iter_num]) : recall_comb})
-    #wandb.log({'f1_score_'+ str(val_img_ids[iter_num]) : f1_comb})
     writer.add_scalar('precision_'+ str(val_img_ids[iter_num]), precision_comb, epoch+1)
     writer.add_scalar('recall_'+ str(val_img_ids[iter_num]), recall_comb, epoch+1)
     writer.add_scalar('f1_score_'+ str(val_img_ids[iter_num]), f1_comb, epoch+1)
 
-    return combined_conf, precision_comb, recall_comb, f1_comb
+    return avg_mae, precision_comb, recall_comb, f1_comb
 
 
 
-def wrt_img(iter_num, actual_list, prev_actual_list, pred_list, val_img_ids):
-    #pred_list = process_val(pred_list)
-    #actual_list = process_val(actual_list)
-    #prev_actual_list = process_val(prev_actual_list)
+def wrt_img(iter_num, actual_list, prev_actual_list, pred_list, val_img_ids,writer):
     
     num_rows = int(pred_list.shape[0])
+
+    actual_ers_lft = np.reshape(np.where(actual_list[:,iter_num,0]<prev_actual_list[:,iter_num,0], 1, 0),(actual_list[:,iter_num,0].shape[0],1))
+    actual_ers_rht = np.reshape(np.where(actual_list[:,iter_num,1]>prev_actual_list[:,iter_num,1], 1, 0),(actual_list[:,iter_num,1].shape[0],1))
+
+    pred_ers_lft = np.reshape(np.where(pred_list[:,iter_num,0]<prev_actual_list[:,iter_num,0], 1, 0),(pred_list[:,iter_num,0].shape[0],1))
+    pred_ers_rht = np.reshape(np.where(pred_list[:,iter_num,1]>prev_actual_list[:,iter_num,1], 1, 0),(pred_list[:,iter_num,1].shape[0],1))
+
+    conf_mat_lft = confusion_matrix(actual_ers_lft, pred_ers_lft)
+    conf_mat_rht = confusion_matrix(actual_ers_rht, pred_ers_rht)
+    combined_conf = conf_mat_lft + conf_mat_rht
+
+    plt_conf_mat(conf_mat_lft, str(val_img_ids[iter_num])+'_conf_mat_left', writer)
+    plt_conf_mat(conf_mat_rht, str(val_img_ids[iter_num])+'_conf_mat_right', writer)
+    plt_conf_mat(combined_conf, str(val_img_ids[iter_num])+'_combined_conf_mat', writer)
 
     denoising = True
     window = 99
     poly = 2
-    pred_left = savgol_filter(pred_list[:,iter_num,0], window, poly)
-    pred_right = savgol_filter(pred_list[:,iter_num,1], window, poly)
+
+    if denoising :
+        pred_left_den = savgol_filter(pred_list[:,iter_num,0], window, poly)
+        pred_right_den = savgol_filter(pred_list[:,iter_num,1], window, poly)
 
     img = cv2.imread('./data/img/up_rgb/'+str(val_img_ids[iter_num])+'.png', 1)
     for i in range(num_rows):
-        if 0<=int(pred_left[i])<=745 or 0<=int(pred_right[i])<=745 :
-            pass
-        else :
-            pred_left[i] = 0
-            pred
 
         img[i,int(actual_list[i,iter_num,0]),:] = [255,255,255]
         img[i,int(actual_list[i,iter_num,1]),:] = [255,255,255]
 
-        img[i,int(prev_actual_list[i,iter_num,0]),:] = [255,0,0]
-        img[i,int(prev_actual_list[i,iter_num,1]),:] = [255,0,0]
-
-        if denoising:
-            img[i,int(pred_left[i]),:] = [0,0,255]
-            img[i,int(pred_right[i]),:] = [0,0,255]
+        if 0<=int(pred_list[i,iter_num,0])<=745 :
+            pass
         else :
-            img[i,int(pred_list[i,iter_num,0]),:] = [0,0,255]
-            img[i,int(pred_list[i,iter_num,1]),:] = [0,0,255]
-    
-    #cv2.imwrite('./data/output/'+str(val_img_ids[iter_num])+'_ot.png', img)
-    #print(img.shape)
+            pred_list[i,iter_num,0] = 0
+        
+        if 0<=int(pred_list[i,iter_num,1])<=745 :
+            pass
+        else :
+            pred_list[i,iter_num,1] = 745
+
+
+        if actual_ers_lft[i] == 1 :
+            img[i,int(prev_actual_list[i,iter_num,0]),:] = [255,0,0]
+            img[i,int(pred_list[i,iter_num,0]),:] = [0,255,0]
+            if denoising:
+                img[i,int(pred_left_den[i]),:] = [0,0,255]
+            
+
+        if actual_ers_rht[i] == 1 :
+            img[i,int(prev_actual_list[i,iter_num,1]),:] = [255,0,0]
+            img[i,int(pred_list[i,iter_num,1]),:] = [0,255,0]
+            if denoising:
+                img[i,int(pred_right_den[i]),:] = [0,0,255]
+            
     writer.add_image(str(val_img_ids[iter_num]), img, dataformats='HWC')
+    return combined_conf
 
 def process_val(arr_list, num_val_img, msk_mean, msk_std):
     arr_list = np.asarray(arr_list)
@@ -212,30 +219,31 @@ def model_save():
 
 def log_performance_metrics(pred_list,actual_list,prev_actual_list,num_val_img, epoch, msk_mean, msk_std, val_img_ids,writer):
     print('logging performance metrics........')
-    pred_list = process_val(pred_list,num_val_img, msk_mean, msk_std)
-    actual_list = process_val(actual_list,num_val_img, msk_mean, msk_std)
-    prev_actual_list = process_val(prev_actual_list,num_val_img, msk_mean, msk_std)
-
+    
     for i in range(num_val_img):
-        #wrt_img(i, actual_list, prev_actual_list, pred_list, val_img_ids)
-        temp_conf,precision_comb,recall_comb,f1_comb = calc_fscore(i, actual_list, prev_actual_list, pred_list, epoch,writer,val_img_ids)
+        pos_mae, precision_comb,recall_comb,f1_comb = calc_fscore(i, actual_list, prev_actual_list, pred_list, epoch,writer,val_img_ids)
         if i == 0 :
-            final_conf = temp_conf
+            test_pos_mae = pos_mae
             final_prec = precision_comb
             final_recall = recall_comb
             final_f1 = f1_comb
         else :
-            final_conf = final_conf + temp_conf
+            test_pos_mae = test_pos_mae + pos_mae
             final_prec = final_prec + precision_comb
             final_recall = final_recall + recall_comb
             final_f1 = final_f1 + f1_comb
 
-    plt_conf_mat(final_conf, 'total_test_confusion_matrix')
-    #wandb.log({"test_set_precision":final_prec/num_val_img, "test_set_recall":final_recall/num_val_img,
-    #            "test_set_f1_score":final_f1/num_val_img})
-    writer.add_scalar("test_set_precision", final_prec/num_val_img, epoch+1)
-    writer.add_scalar("test_set_recall", final_recall/num_val_img, epoch+1)
-    writer.add_scalar("test_set_f1_score", final_f1/num_val_img, epoch+1)
+    test_pos_mae = test_pos_mae/num_val_img
+    test_prec = final_prec/num_val_img
+    test_recall = final_recall/num_val_img
+    test_f1_score = final_f1/num_val_img
+
+    writer.add_scalar("test_set_pos_mae", test_pos_mae, epoch+1)
+    writer.add_scalar("test_set_precision", test_prec, epoch+1)
+    writer.add_scalar("test_set_recall", test_recall, epoch+1)
+    writer.add_scalar("test_set_f1_score", test_f1_score, epoch+1)
+
+    return test_pos_mae, test_prec, test_recall, test_f1_score
 
 
 def objective(trial):
@@ -245,23 +253,23 @@ def objective(trial):
     num_lstm_layers = 1
     num_channels = 7
     batch_size = 100
-    EPOCHS = 12
+    EPOCHS = 16
     lr_rate = trial.suggest_loguniform('lr_rate', 1e-5, 1e2)                       #.0001
-    in_seq_num = 20
-    output_at = 10
-    model_type = 'CNN_Model_fix'
+    #in_seq_num = 20
+    #output_at = 10
+    model_type = 'CNN_Model_vanilla'
     drop_rate = 0.25
     time_step = 5
     val_batch_size = batch_size
     total_time_step = 33
     data_div_step = 31
 
-    log_performance = 10   ###number of epochs after which performance metrics are calculated
+    log_performance = 4 ###number of epochs after which performance metrics are calculated
     model_save_at = 50     ###number of epochs after which to save model
     early_stop_thresh = 30
     val_img_ids = [201901, 202001]
     num_val_img = len(val_img_ids)
-    log_hist = 10
+    log_hist = 5
 
     hyperparameter_defaults = dict(
         dropout = str(drop_rate),
@@ -273,19 +281,12 @@ def objective(trial):
         num_lstm_layers = num_lstm_layers,
         total_window = total_window,
         dataset='7_chann',
-        model_type=model_type
+        model_type=model_type,
+        vertical_image_window = 1
         )
 
-    # WandB – Initialize a new run
-    #wandb.init(entity="antor", project="bank_line", config=hyperparameter_defaults)
-    #wandb.init(entity="antor",project="bank_final", config=hyperparameter_defaults)
     writer = SummaryWriter(log_dir='./runs/lr_rate/')
 
-
-    # WandB – Config is a variable that holds and saves hyperparameters and inputs
-    #config = wandb.config          # Initialize config
-
-    #config.update({'dataset':'7_chann','model_type':model_type})
 
     dataset_f = tf.data.TFRecordDataset('./data/tfrecord/comp_tf.tfrecords')
     dataset_f = dataset_f.window(size=data_div_step, shift=total_time_step, stride=1, drop_remainder=False)
@@ -304,7 +305,6 @@ def objective(trial):
 
     model = CNN_Model(num_channels, batch_size, val_batch_size,time_step, num_lstm_layers, drop_rate)
 
-    #use_cuda = not False and torch.cuda.is_available()
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print(device)
     model = model.to(device)
@@ -316,7 +316,6 @@ def objective(trial):
         model.load_state_dict(checkpoint['model_state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
-    #wandb.watch(model, log="all")
 
     msk_mean = np.load('./data/mean_img/line_mean.npy')
     msk_std = np.load('./data/mean_img/line_std.npy')
@@ -356,7 +355,6 @@ def objective(trial):
         template = 'Epoch {}, Train Loss: {}'
         print(template.format(epoch+1,avg_epoch_loss))
 
-        #wandb.log({"Train Loss": avg_epoch_loss})
         writer.add_scalar('Loss/train', avg_epoch_loss, epoch+1)
 
         if epoch == 0 :
@@ -378,14 +376,14 @@ def objective(trial):
         counter_val = 0
         
         with torch.no_grad():
-
-            pred_list = []
-            actual_list = []
-            prev_actual_list = []
+            
+            if epoch % log_performance == log_performance-1:
+                pred_list = []
+                actual_list = []
+                prev_actual_list = []
 
             for input_tensor, reg_coor, _ , year_id in dataseti1:
-                #print(year_id)
-                #print(asd)
+
                 input_tensor = np.reshape(input_tensor, (batch_size,time_step,745,num_channels))
                 reg_coor = np.reshape(reg_coor, (batch_size,time_step,2))
 
@@ -415,7 +413,6 @@ def objective(trial):
             template = 'Epoch {}, Val_Loss: {}'
             print(template.format(epoch+1,avg_val_epoch_loss))
 
-            #wandb.log({"Val_Loss": avg_val_epoch_loss})
             writer.add_scalar('Loss/Val', avg_val_epoch_loss, epoch+1)
 
             if epoch == 0 :
@@ -427,20 +424,30 @@ def objective(trial):
                 else :
                     early_stop_counter += 1
 
-
             ###logging performance metrics
             if epoch % log_performance == log_performance-1 :
-                log_performance_metrics(pred_list,actual_list,prev_actual_list,num_val_img, epoch, msk_mean, msk_std, val_img_ids,writer)
+                pred_list = process_val(pred_list,num_val_img, msk_mean, msk_std)
+                actual_list = process_val(actual_list,num_val_img, msk_mean, msk_std)
+                prev_actual_list = process_val(prev_actual_list,num_val_img, msk_mean, msk_std)
+                test_pos_mae, test_prec, test_recall, test_f1_score = log_performance_metrics(pred_list,actual_list,prev_actual_list,
+                                                    num_val_img, epoch, msk_mean, msk_std, val_img_ids,writer)
                 
         if early_stop_counter > early_stop_thresh :
             print('early stopping as val loss is not improving ........')
             model_save()
             break
 
-    #for i in range(num_val_img):
-    #    wrt_img(i, actual_list, prev_actual_list, pred_list, val_img_ids)
+    for iter_num in range(num_val_img):
+        temp_conf = wrt_img(iter_num, actual_list, prev_actual_list, pred_list, val_img_ids, writer)
 
-    writer.add_hparams(hyperparameter_defaults,{'hparam/train_loss':avg_epoch_loss,'hparam/val_loss':avg_val_epoch_loss})
+        if iter_num == 0 :
+            final_conf = temp_conf
+        else :
+            final_conf = final_conf + temp_conf
+        
+    plt_conf_mat(final_conf, 'total_test_confusion_matrix', writer)
+    writer.add_hparams(hyperparameter_defaults,{'hparam/train_loss':avg_epoch_loss,'hparam/val_loss':avg_val_epoch_loss,
+    'hparam/pos_mae':test_pos_mae, 'hparam/precision':test_prec,'hparam/recall':test_recall,'hparam/f1_score':test_f1_score})
     writer.close()
 
     return avg_val_epoch_loss
@@ -448,5 +455,5 @@ def objective(trial):
 
 if __name__ == "__main__":
     study = optuna.create_study()
-    study.optimize(objective, n_trials=4) 
+    study.optimize(objective, n_trials=1) 
     pass
